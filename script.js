54421dc4edc4e71eafd1b120c682c7df3f52b7bb
kumarmanojgit <krmanoj72099@gmail.com>
2024-12-26 10:43:55 +0530

    laod all containent

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const searchButton = document.getElementById("search-btn");
+  const usernameInput = document.getElementById("user-input");
+  const statsContainer = document.querySelector(".stats-container");
+  const easyProgressCircle = document.querySelector(".easy-progress");
+  const mediumProgressCircle = document.querySelector(".medium-progress");
+  const hardProgressCircle = document.querySelector(".hard-progress");
+  const easyLabel = document.getElementById("easy-label");
+  const mediumLabel = document.getElementById("medium-label");
+  const hardLabel = document.getElementById("hard-label");
+  const cardStatsContainer = document.querySelector(".stats-cards");
+
+  //return true or false based on a regex
+  function validateUsername(username) {
+    if (username.trim() === "") {
+      alert("Username should not be empty");
+      return false;
+    }
+    const regex = /^[a-zA-Z0-9_-]{1,15}$/;
+    const isMatching = regex.test(username);
+    if (!isMatching) {
+      alert("Invalid Username");
+    }
+    return isMatching;
+  }
+  searchButton.addEventListener("click", function () {
+    const username = usernameInput.value;
+    console.log("logggin username: ", username);
+    if (validateUsername(username)) {
+      fetchUserDetails(username);
+    }
+  });
+});
